ab855f8ed2713e0450eff9536edba940a883258e
ivan.cheung <inhcheung@gmail.com>
2016-04-19 23:47:31 +0100

    made pattern matching work with getting the href tag and stripping off the domain name, even though I could've just used the 'text', just practice on regexp

--- lib/browsercontainer.rb
+++ lib/browsercontainer.rb
@@ -11,4 +11,21 @@ class BrowserContainer
     @browser.url
   end
 
+  def site_data_links
+    @browser.links
+  end
+
+  def list_site_data(datatype)
+    #images = []
+    #open folder
+    @browser.goto ("#{ENV['HOST']}/#{datatype}/")
+
+    #scrape links to array
+    site_data_links.each do |link|
+     #puts link.href.gsub(/http:\/\/192.168.88.105:8000\/#{datatype}/,'<matched here>')
+     puts link.href.gsub(/[a-z]+:\/\/\d+.\d+.\d+.\d+:\d+\/#{datatype}/,'')
+    end
+    #return images
+  end
+
 end
